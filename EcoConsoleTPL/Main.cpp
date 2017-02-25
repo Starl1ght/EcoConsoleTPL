bac21e9c66a70b662c7b0e89387b42b403b80697
Denis Panin <starl1ght@mail.ru>
2017-02-25 20:28:53 +0300

    A couple of oopses

--- EcoConsoleTPL/Main.cpp
+++ EcoConsoleTPL/Main.cpp
@@ -40,7 +40,7 @@ template <typename ARGT1, typename ARGT2>
 struct Invoke<2, ARGT1, ARGT2> {
 	template <typename CALLABLE>
 	static void Do(CALLABLE func, const std::vector<std::string>& tokens) {
-		const auto& arg1 = Converter<ARGT2>(tokens[1]);
+		const auto& arg1 = Converter<ARGT1>(tokens[1]);
 		const auto& arg2 = Converter<ARGT2>(tokens[2]);
 		func(arg1, arg2);
 	}
@@ -53,7 +53,7 @@ struct HelpHelper {};
 template <typename CMD> 
 struct HelpHelper<2, CMD> {
 	static void Do(std::stringstream& ss) {
-		ss << " TYPES: " << typeid(CMD::ArgType1).name() << ' ' << typeid(CMD::ArgType1).name();
+		ss << " TYPES: " << typeid(CMD::ArgType1).name() << ' ' << typeid(CMD::ArgType2).name();
 	}
 };
 
