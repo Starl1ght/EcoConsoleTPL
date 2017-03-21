4373d2fc227123d537e4447183a0326609dda5eb
Denis Panin <Starl1ght@users.noreply.github.com>
2017-03-21 16:50:28 +0300

    Formatting #2

--- EcoConsoleTPL/Callbacks.h
+++ EcoConsoleTPL/Callbacks.h
@@ -2,19 +2,19 @@
 #include <iostream>
 
 namespace fn {
-    void sum(const int& a, const int& b) {
-        std::cout << a << " + " << b << " = " << a + b << '\n';
-    }
-    void div(const double& a, const double& b) {
-        std::cout << a << " / " << b << " = " << a / b << '\n';
-    }
-    void square(const unsigned long& a) {
-        std::cout << a << " ^ 2 = " << a * a << '\n';
-    }
-    void reverse(const std::string& str) {
-        std::cout << str << " -> " << std::string{ str.rbegin(), str.rend() } << '\n';
-    }
-    void con_exit() {
-        exit(0);
-    }
+	void sum(const int& a, const int& b) {
+		std::cout << a << " + " << b << " = " << a + b << '\n';
+	}
+	void div(const double& a, const double& b) {
+		std::cout << a << " / " << b << " = " << a / b << '\n';
+	}
+	void square(const unsigned long& a) {
+		std::cout << a << " ^ 2 = " << a * a << '\n';
+	}
+	void reverse(const std::string& str) {
+		std::cout << str << " -> " << std::string{ str.rbegin(), str.rend() } << '\n';
+	}
+	void con_exit() {
+		exit(0);
+	}
 }
